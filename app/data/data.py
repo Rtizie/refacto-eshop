add7a70b709534de023c94c3d88e169d99ed3fad
Alex <sasahrac@gmail.com>
2021-09-21 19:01:11 +0200

    FUCKING SQL

--- app/data/data.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def getData():
-    with open("./app/data/data.json", 'r') as f:
-        data = json.load(f)
-        print(type(data))
-        return data
--- app/main.py
+++ app/main.py
@@ -1,11 +1,29 @@
 from flask import Flask,render_template
 import sass
+from sqlalchemy.sql.expression import null
+from werkzeug.exceptions import abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///clothes.db"
 
-from app.data.data import *
+db = SQLAlchemy(app)
 
-sass.compile(dirname=('app/static/scss', 'app/static/css'))
+class Shirt(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(40),unique=True, nullable=False)
+        color = db.Column(db.String(100), nullable=False)
+        size = db.Column(db.String(20), nullable=False)
+        cost = db.Column(db.Integer, nullable=False)
+        stock = db.Column(db.Integer, nullable=False)
+
+        def __repr__(self) -> str:
+            return f'Name:{self.name}, Color:{self.color}, Size:{self.size}, Cost:{self.cost}, Stock:{self.stock}'
+
+
+#@app.errorhandler(404)
+#def page_not_found(error):
+#   return render_template('404.html', title = '404'), 404
 
 
 @app.route("/")
@@ -18,7 +36,6 @@ def kolekce():
         sass.compile(dirname=('app/static/scss', 'app/static/css'))
         return render_template('kolekce.html')
 
-@app.route("/test")
+@app.route("/database")
 def testPage():
-        data = getData()
-        return render_template('test.html', var=data['shirts']['kekw'][0]['size'])
+        return render_template('data.html',var=Shirt.query.all()[0].cost)
