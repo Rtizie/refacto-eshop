7aa831df68df7a51446e0d5ede59924ba97f27a0
Alexandr Merunka <sasahrac@gmail.com>
2021-12-04 11:47:26 +0100

    FIx

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///clothes.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = b'\xc36@\xa8\x80\x0bWO\x04\xb7\xdc\xc8\xdd3\xa4\xa2\xe9\xaeV\x0bd\xf9\x98\xde'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
-logging.getLogger('werkzeug').setLevel('ERROR')
+#logging.getLogger('werkzeug').setLevel('ERROR')
 db = SQLAlchemy(app)
 
 class Shirt(db.Model):
